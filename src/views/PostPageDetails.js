f2ca1b3a295aadea46a7726fe1b798e459f9be68
linjunjie <linjunjie.1995@gmail.com>
2023-08-23 21:44:46 +0800

    Bugfix for the add, edit post features

--- src/views/PostPageDetails.js
+++ src/views/PostPageDetails.js
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { Button, ButtonGroup, Card, Col, Container, Image, ListGroup, ListGroupItem, Nav, Navbar, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Nav,
+  Navbar,
+  Row,
+} from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db, storage } from "../firebase";
 import { signOut } from "firebase/auth";
-import { deleteDoc, doc, getDoc, collection, getDocs, where, query, updateDoc } from "firebase/firestore";
+import {
+  deleteDoc,
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+  where,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 import { ref, deleteObject } from "firebase/storage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -15,9 +35,9 @@ export default function PostPageDetails() {
   const [comments, setComments] = useState([]);
   const [commentStatus, setCommentStatus] = useState([]);
   const [likeStatus, setLikeStatus] = useState(false);
-  const [likeCounter, setLikeCounter] =useState(0);
-  const [userId, setUserId] = useState("")
-  const [reviewId, setReviewId] = useState("");
+  const [userLikes, setUserLikes] = useState([]);
+  const [likeCounter, setLikeCounter] = useState(0);
+  const [userId, setUserId] = useState("");
   const params = useParams();
   const id = params.id;
   const [user, loading] = useAuthState(auth);
@@ -37,93 +57,111 @@ export default function PostPageDetails() {
     navigate("/");
   }
 
-  async function getPost(id) {
+  async function getPost(id, user) {
     const postDocument = await getDoc(doc(db, "posts", id)); //get this post from the db called posts
     const post = postDocument.data();
-    let uId = ""
     setCaption(post.caption);
     setImage(post.image);
     setImageName(post.imageName);
     setComments(post.comments);
     setLikeCounter(post.likes);
+    const status = [];
 
-    for(let i = 0; i < post.comments.length; i++){
-      let status = commentStatus;
+    for (let i = 0; i < post.comments.length; i++) {
       status.push(false);
     }
+    setCommentStatus(status);
 
-    const userEmail = auth.currentUser.email;
-    const q =  await getDocs(query(collection(db, "users"), where("email", "==", userEmail)));
-    
-    //const postReview = await getDocs(collection(db, "users", id, "reviews"));   
-    q.forEach((doc) =>{
-      setUserId(doc.id);
-      uId=doc.id;
-    })
-    const postReview = await getDocs(collection(db, "users", uId, "reviews"));
-    postReview.forEach((doc) => {
-      let d = doc.data()
-      if(d.postId === id){
-        setReviewId(doc.id);
-        setLikeStatus(d.like);
-      }
-    })
-    
-  }
+    if (user) { // if user is login, retrieve the user info from the db whcih contain user email and array of postId which the user like
+      const q = await getDocs(
+        query(collection(db, "users"), where("email", "==", auth.currentUser.email))
+      );
 
-  const handleLike = async (e) => {
-    let counter = likeCounter;    
-    
-      if(likeStatus){
-        counter -= 1;
-      } else {
-        counter +=1;
-      }
-    
-    if(counter === 0 || counter > 0){
-      setLikeCounter(counter);
-      await updateDoc(doc(db, "posts", id), { likes: counter});
+      q.forEach((doc) => {
+        const item = doc.data();
+        setUserId(doc.id);
+        setUserLikes(item.likes);
+        if((item.likes === undefined || item.likes.length > 0) && item.likes.includes(id)){
+          setLikeStatus(true)
+        }
+      });
     }
-    await updateDoc(doc(db, "users", userId, "reviews", reviewId ), { like: !likeStatus  });
-
   }
 
-  const toggleComment = async(index) => { 
-    let status = commentStatus;
+
+  const handleLike = async (e) => { // toggle and send the like status to the db
+    let counter = likeCounter;
+    let userLikesArray = userLikes;
+
+    if (likeStatus) {
+      counter -= 1;
+      userLikesArray.splice(userLikes.indexOf(id), 1)
+    } else {
+      counter += 1;
+      userLikesArray.push(id)
+    }
+
+    console.log(userLikesArray + " _ " + userId)
+
+    if (counter === 0 || counter > 0) {
+      setLikeCounter(counter);
+      await updateDoc(doc(db, "posts", id), { likes: counter });
+    }
+
+    await updateDoc(doc(db, "users", userId), {
+      likes: userLikesArray
+    });
+  };
+
+  const toggleComment = async (index) => { //save to db if comment is text 
+    let newComments = [...comments];
+    let status = [...commentStatus];
     status[index] = !commentStatus[index];
-    setCommentStatus(status)
-  }
+    setCommentStatus(status);
 
-  const handleDeleteComment = async(index) => {
-  let newComments = comments;
+    if(status[index]===false){
+      newComments.splice(index, {userid: userId, comment:comments[index] })
+      await updateDoc(doc(db, "posts", id), { comments: newComments});
+    }
+  };
 
-  newComments.splice(index, 1);
+  const handleDeleteComment = async (index) => { // delete comment object from the comments array and the comment array in the db
+    let newComments = [...comments];
+    let newCommentStatus = [...commentStatus];
 
-  await updateDoc(doc(db, "posts", id), {comments: newComments});
-  }
+    newComments.splice(index, 1);
+    newCommentStatus.splice(index, 1);
 
-  const handleUpdateComment = async(e, index) => {
-    let newComments = comments;
-    
+    setComments(newComments);
+    setCommentStatus(newCommentStatus);
 
-  newComments.splice(index, 1, e.target.value);
-  setComments(newComments)
-    await updateDoc(doc(db, "posts", id), {comments: newComments});
-  }
+    await updateDoc(doc(db, "posts", id), { comments: newComments });
+  };
 
-  const handleAddComment = async() => {
-    let newComments = comments;
-    
-  newComments.push("");
-  setComments(newComments)
-    await updateDoc(doc(db, "posts", id), {comments: newComments});
-  }
+  const handleUpdateComment = async (e, index) => { //change the value in the comment object
+    let newComments = [...comments];
+
+    newComments.splice(index, 1, {userId: userId, comment: e.target.value});
+    setComments(newComments);
+    console.log(newComments)
+  };
+
+  const handleAddComment = async () => { // add a new comment object to the comments array
+    let newComments = [...comments];
+    let newCommentStatus = [...commentStatus];
+
+    newComments.push({userId: userId, comment:""});
+    await updateDoc(doc(db, "posts", id), { comments: newComments });
+    newCommentStatus.push(false)
+    setComments(newComments);
+    setCommentStatus(newCommentStatus);
+  };
 
   useEffect(() => {
     if (loading) return;
     if (!user) navigate("/login");
-    getPost(id);
-  }, [id, navigate, user, loading, comments,commentStatus]);
+    getPost(id, user, );
+  }, [id, navigate, user, loading]);
 
   return (
     <>
@@ -154,28 +192,53 @@ export default function PostPageDetails() {
                   size="lg"
                   onClick={(e) => {
                     setLikeStatus(!likeStatus);
-                    handleLike(e)
+                    handleLike(e);
                   }}
-                  style={{ cursor: "pointer", paddingRight: "16px", color:"blue" }}
+                  style={{
+                    cursor: "pointer",
+                    paddingRight: "16px",
+                    color: "blue",
+                  }}
                 />
                 <Card.Link href={`/update/${id}`}>Edit</Card.Link>
-                <Card.Link 
+                <Card.Link
                   onClick={() => deletePost(id)}
                   style={{ cursor: "pointer" }}
                 >
                   Delete
                 </Card.Link>
-                <ListGroup style={{paddingTop: '10px'}}>
-                  {comments.map((comment, index) => (
+                <ListGroup style={{ paddingTop: "10px" }}>
+                  {comments.map((item, index) => (
                     <ListGroupItem key={index}>
-                      {!commentStatus[index] ? <p>{comment}</p> : <input value={comment} onChange={(e)=> handleUpdateComment(e, index)} />}
-                      <ButtonGroup>
-                      <Button onClick={()=> toggleComment(index)}>Edit</Button>
-                      <Button onClick={() => {handleDeleteComment(index)}}>Delete</Button>
-                      </ButtonGroup>
+                      {!commentStatus[index] ? (
+                        <p>{item.comment}</p>
+                      ) : (
+                        <input
+                          value={item.comment}
+                          onChange={(e) => handleUpdateComment(e, index)}
+                        />
+                      )}
+                        {item.userId === userId ?<><Card.Link  style={{ cursor: "pointer" }} onClick={() => toggleComment(index)}>
+                          Edit
+                        </Card.Link>
+                        <Card.Link
+                         style={{ cursor: "pointer" }}
+                          onClick={() => {
+                            handleDeleteComment(index);
+                          }}
+                        >
+                          Delete
+                        </Card.Link></>: ""}
                     </ListGroupItem>
                   ))}
-                  <Button onClick={() => {handleAddComment()}}>Add</Button>
+                  <Button
+                    className="mt-2"
+                    onClick={() => {
+                      handleAddComment();
+                    }}
+                  >
+                    Add
+                  </Button>
                 </ListGroup>
               </Card.Body>
             </Card>
